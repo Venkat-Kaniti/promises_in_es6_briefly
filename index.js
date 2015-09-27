78ff8344151cd341811eba78f56eda4d90f48514
Venkat Kaniti <venkat.kaniti@users.noreply.github.com>
2015-09-27 01:24:48 -0400

    Promise version

--- index.js
+++ index.js
@@ -1,44 +1,58 @@
 'use strict';
 
 // Get cookie dough
-var GetCookieDough = function (cash, cb) {
-	var doughPerOneDollar = 2;
-	setTimeout(function () {
-		cb(null, cash * doughPerOneDollar);
-	}, 1000);
+let GetCookieDough = (cash) => {
+
+	let doughPerOneDollar = 2;
+	let executor = (resolve, reject) => {
+
+		setTimeout(() => {
+			resolve(cash * doughPerOneDollar);
+		}, 1000);
+
+	};
+
+	return new Promise(executor);
 };
 
 // Make cookies from dough
-var MakeCookiesFromDough = function (dough, cb) {
-	var cookiesPerOneLbDough = 15;
-	setTimeout(function () {
-		cb(null, dough * cookiesPerOneLbDough);
-	}, 1000);
+let MakeCookiesFromDough = (dough) => {
+
+	let cookiesPerOneLbDough = 15;
+	let executor = (resolve, reject) => {
+		setTimeout(() => {
+			resolve(dough * cookiesPerOneLbDough);
+		}, 1000);
+	};
+
+	return new Promise(executor);
+
 };
 
-// Sell cookies and make money
-var SellCookies = function (cookies, cb) {
-	var priceOfOneCookie = 0.5;
-	setTimeout(function () {
-		cb(null, cookies * priceOfOneCookie);
-	}, 1000);
+//Sell cookies and make money
+let SellCookies = (cookies) => {
+
+	let priceOfOneCookie = 0.5;
+	let executor = (resolve, reject) => {
+		setTimeout(() => {
+			resolve(cookies * priceOfOneCookie);
+		}, 1000);
+	};
+
+	return new Promise(executor);
+
 };
 
-
-//// CALLBACK HELL
-	
-// Start cookie business
-var investment = process.argv[2] || 10;
-GetCookieDough(investment, function (err, dough) {
-
-	MakeCookiesFromDough(dough, function (err, cookies) {
-
-		SellCookies(cookies, function (err, returns) {
-
-			console.log('$'+ investment + ' invested.' + ' $' + returns + ' worth of cookies sold');
-
-		});
-	})
-});
+//Start cookie business
 
 console.log('Started cookie business..!');
+
+let investment = process.argv[2] || 10;
+
+Promise.resolve(investment)
+	.then(GetCookieDough)
+	.then(MakeCookiesFromDough)
+	.then(SellCookies)
+	.then(result => {
+		console.log(`$${investment} invested. $${result} worth of cookies sold`);
+	});
